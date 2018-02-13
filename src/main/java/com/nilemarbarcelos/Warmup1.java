a751dd5cd6c106fab3c20637ca5f89f0eb677528
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2018-02-12 22:37:52 -0200

    Challenges

--- src/main/java/com/nilemarbarcelos/Warmup1.java
+++ src/main/java/com/nilemarbarcelos/Warmup1.java
@@ -62,5 +62,158 @@ public class Warmup1 {
         return false;
     }
 
+    /*
+        Given an int n, return true if it is within 10 of 100 or 200.
+        Note: Math.abs(num) computes the absolute value of a number.
+    */
+    public boolean nearHundred(int n) {
+        return n >= 90 && n <= 110 || n >= 190 && n <= 210;
+    }
+
+    /*
+        Given 2 int values, return true if one is negative and one is positive.
+        Except if the parameter "negative" is true, then return true only if both are negative.
+    */
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative)
+            return a < 0 && b < 0;
+        else
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+    }
+
+    /*
+        Given a string, return a new string where "not " has been added to the front.
+        However, if the string already begins with "not", return the string unchanged.
+        Note: use .equals() to compare 2 strings.
+    */
+    public String notString(String str) {
+        String[] split = str.split(" ");
+        if ("not".equals(split[0]))
+            return str;
+        return "not " + str;
+    }
+
+
+    /*
+        Given a non-empty string and an int n,
+        return a new string where the char at index n has been removed.
+        The value of n will be a valid index of a char in the original string
+        (i.e. n will be in the range 0..str.length()-1 inclusive).
+    */
+    public String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+        String back = str.substring(n+1, str.length());
+        return front + back;
+    }
+
+    /*
+        Given a string, return a new string where the first and last chars have been exchanged.
+    */
+    public String frontBack(String str) {
+        if (str.length() <= 1) return str;
+        char first = str.charAt(0);
+        char last = str.charAt(str.length() - 1);
+        String middle = str.substring(1, str.length() - 1);
+        return last + middle + first;
+    }
+
+    /*
+        Given a string, we'll say that the front is the first 3 chars of the string.
+        If the string length is less than 3, the front is whatever is there.
+        Return a new string which is 3 copies of the front.
+    */
+    public String front3(String str) {
+        str = str.length() < 3 ? str : str.substring(0, 3);
+        String newStr = "";
+        for (int i = 0; i < 3; i++) {
+            newStr += str;
+        }
+        return newStr;
+    }
+
+    /*
+        Given a string, take the last char and return a new string with the last char added at the front and back,
+        so "cat" yields "tcatt". The original string will be length 1 or more.
+    */
+    public String backAround(String str) {
+        char last = str.charAt(str.length() - 1);
+        return last + str + last;
+    }
+
+    /*
+        Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+        Use the % "mod" operator
+    */
+    public boolean or35(int n) {
+        return n % 3 == 0 || n % 5 == 0;
+    }
+
+    /*
+        Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back,
+        so "kitten" yields"kikittenki".
+        If the string length is less than 2, use whatever chars are there.
+    */
+    public String front22(String str) {
+        String first2 = str.length() < 2 ? str : str.substring(0, 2);
+        return first2 + str + first2;
+    }
+
+    /*
+        Given a string, return true if the string starts with "hi" and false otherwise.
+    */
+    public boolean startHi(String str) {
+        if (str.length() < 2) return false;
+        return str.substring(0, 2).equals("hi");
+    }
+
+    /*
+        Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+    */
+    public boolean icyHot(int temp1, int temp2) {
+        return ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0));
+    }
+
+    /*
+        Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+    */
+    public boolean in1020(int a, int b) {
+        return a >= 10 && a <= 20 || b >= 10 && b <= 20;
+    }
+
+    /*
+        We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+        Given 3 int values, return true if 1 or more of them are teen.
+    */
+    public boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19 || b >= 13 && b <= 19 || c >= 13 && c <= 19);
+    }
+
+    /*
+        We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+        Given 2 int values, return true if one or the other is teen, but not both.
+    */
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = a >= 13 && a <= 19;
+        boolean bTeen = b >= 13 && b <= 19;
+        return aTeen && !bTeen || bTeen && !aTeen;
+    }
+
+    /*
+        Given a string, if the string "del" appears starting at index 1,
+        return a string where that "del" has been deleted.
+        Otherwise, return the string unchanged.
+    */
+    public String delDel(String str) {
+        if (str.length() < 4) return str;
+        String newStr = str.substring(1, 4);
+        char first = str.charAt(0);
+        if ("del".equals(newStr))
+            return first + str.substring(4, str.length());
+        return str;
+    }
+
+
+
+
 
 }
--- src/test/java/com/nilemarbarcelos/Warmup1Test.java
+++ src/test/java/com/nilemarbarcelos/Warmup1Test.java
@@ -31,16 +31,16 @@ public class Warmup1Test {
 
     @Test
     public void sumDouble() {
-        assertEquals(warmup.sumDouble(1, 2), 3);
-        assertEquals(warmup.sumDouble(3, 2), 5);
-        assertEquals(warmup.sumDouble(2, 2), 8);
+        assertEquals(3, warmup.sumDouble(1, 2));
+        assertEquals(5, warmup.sumDouble(3, 2));
+        assertEquals(8, warmup.sumDouble(2, 2));
     }
 
     @Test
     public void diff21() {
-        assertEquals(warmup.diff21(19), 2);
-        assertEquals(warmup.diff21(10), 11);
-        assertEquals(warmup.diff21(21), 0);
+        assertEquals(2, warmup.diff21(19));
+        assertEquals(11, warmup.diff21(10));
+        assertEquals(0, warmup.diff21(21));
     }
 
     @Test
@@ -55,5 +55,110 @@ public class Warmup1Test {
         assertTrue(warmup.makes10(9, 10));
         assertFalse(warmup.makes10(9, 9));
         assertTrue(warmup.makes10(1, 9));
+    }
+
+    @Test
+    public void nearHundred() {
+        assertTrue(warmup.nearHundred(93));
+        assertTrue(warmup.nearHundred(90));
+        assertFalse(warmup.nearHundred(89));
+    }
+
+    @Test
+    public void posNeg() {
+        assertTrue(warmup.posNeg(1, -1, false));
+        assertTrue(warmup.posNeg(-1, 1, false));
+        assertTrue(warmup.posNeg(-4, -5, true));
+    }
+
+    @Test
+    public void notString() {
+        assertEquals("not candy", warmup.notString("candy"));
+        assertEquals("not x", warmup.notString("x"));
+        assertEquals("not bad", warmup.notString("not bad"));
+    }
+
+    @Test
+    public void missingChar() {
+        assertEquals("ktten", warmup.missingChar("kitten", 1));
+        assertEquals("itten", warmup.missingChar("kitten", 0));
+        assertEquals("kittn", warmup.missingChar("kitten", 4));
+    }
+
+    @Test
+    public void frontBack() {
+        assertEquals("eodc", warmup.frontBack("code"));
+        assertEquals("a", warmup.frontBack("a"));
+        assertEquals("ab", warmup.frontBack("ba"));
+    }
+
+    @Test
+    public void front3() {
+        assertEquals("JavJavJav", warmup.front3("Java"));
+        assertEquals("ChoChoCho", warmup.front3("Chocolate"));
+        assertEquals("abcabcabc", warmup.front3("abc"));
+    }
+
+    @Test
+    public void backAround() {
+        assertEquals("tcatt", warmup.backAround("cat"));
+        assertEquals("oHelloo", warmup.backAround("Hello"));
+        assertEquals("aaa", warmup.backAround("a"));
+    }
+
+    @Test
+    public void or35() {
+        assertTrue(warmup.or35(3));
+        assertTrue(warmup.or35(10));
+        assertFalse(warmup.or35(8));
+    }
+
+    @Test
+    public void front22() {
+        assertEquals("kikittenki", warmup.front22("kitten"));
+        assertEquals("HaHaHa", warmup.front22("Ha"));
+        assertEquals("ababcab", warmup.front22("abc"));
+    }
+
+    @Test
+    public void startHi() {
+        assertTrue(warmup.startHi("hi there"));
+        assertTrue(warmup.startHi("hi"));
+        assertFalse(warmup.startHi("hello hi"));
+    }
+
+    @Test
+    public void icyHot() {
+        assertTrue(warmup.icyHot(120, -1));
+        assertTrue(warmup.icyHot(-1, 120));
+        assertFalse(warmup.icyHot(2, 120));
+    }
+
+    @Test
+    public void in1020() {
+        assertTrue(warmup.in1020(12, 99));
+        assertTrue(warmup.in1020(21, 12));
+        assertFalse(warmup.in1020(8, 99));
+    }
+
+    @Test
+    public void hasTeen() {
+        assertTrue(warmup.hasTeen(13, 20, 10));
+        assertTrue(warmup.hasTeen(20, 19, 10));
+        assertTrue(warmup.hasTeen(20, 10, 13));
+    }
+
+    @Test
+    public void loneTeen() {
+        assertTrue(warmup.loneTeen(13, 99));
+        assertTrue(warmup.loneTeen(21, 19));
+        assertFalse(warmup.loneTeen(13, 13));
+    }
+
+    @Test
+    public void delDel() {
+        assertEquals("abc", warmup.delDel("adelbc"));
+        assertEquals("aHello", warmup.delDel("adelHello"));
+        assertEquals("adedbc", warmup.delDel("adedbc"));
     }
 }
